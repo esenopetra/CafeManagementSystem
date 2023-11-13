d2744fc061d6e3ec4b0e37f6739aee02e3e1ee57
Abhinav Selvarajan <91642109+esenopetra@users.noreply.github.com>
2023-11-13 12:18:46 +0530

    Delete Completed directory

--- Completed/quizapp/quizapp/src/main/java/com/abhinav/quizapp/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.abhinav.quizapp.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
